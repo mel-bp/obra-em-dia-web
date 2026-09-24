b046edcaad2ef8da765d747ef3a11dde4618c215
Melina BP <mel-bp@users.noreply.github.com>
2026-09-23 21:55:37 -0300

    Publish Obra em Dia web app

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='obra-em-dia-v1';
+const APP_FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(new URL(event.request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy))}return response}))) });
---
+++ supabase/config.js
@@ -0,0 +1,5 @@
+// Chave publicável: pode ser incluída no cliente. RLS protege os dados.
+window.OBRA_SUPABASE = {
+  url: 'https://gtbzawhsatifcjcpliyc.supabase.co',
+  publishableKey: 'sb_publishable_ioZ9qy7mmhSbJIWqN97XOQ_P42Gu6r5'
+};
